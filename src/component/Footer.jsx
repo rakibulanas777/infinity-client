cfc184e3d1f5b19a50bd3ec7efdaaa4d8cceea76
Rakibul islam Anas <rakibulanas777@gmail.com>
2023-11-17 20:34:28 +0600

    add

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,30 +3,30 @@ import { FaFacebookF } from "react-icons/fa";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { BsInstagram } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import './Footer.css'
 const Footer = () => {
   return (
     <div className="border-t-2">
-      <div className="container mx-auto py-6 px-10 sm:px-8 md:px-6 lg:px-10">
-        <div className="flex items-center flex-col md:flex-row md:space-y-0 space-y-4 justify-between pt-5 pb-5 ">
-          <Link
-            className="text-2xl text-black font-bold cursor-pointer hover:text-red-500"
-            href="/"
-          >
-            infinity24
-          </Link>
-          <div className="medium flex space-x-3">
-            <div className="cursor-pointer font-semibold">Home</div>
-            <div className="cursor-pointer font-semibold">Home</div>
-            <div className="cursor-pointer font-semibold">Home</div>
-            <div className="cursor-pointer font-semibold">Home</div>
-            <div className="cursor-pointer font-semibold">Home</div>
-          </div>
-          <div className="flex space-x-3">
-            <FaFacebookF />
-            <AiOutlineTwitter />
-            <BsInstagram />
-          </div>
+      <div className="container flex items-center justify-center flex-col space-y-6 mx-auto py-6 px-10 sm:px-8 md:px-6 lg:px-10">
+        <div className="footer-links text-xl text-black">
+          <a href="#">About</a>
+          <a href="#">Store locator</a>
+          <a href="#">FAQs</a>
+          <a href="#">News</a>
+          <a href="#">Careers</a>
+          <a href="#">Contact Us</a>
         </div>
+        <p className="love text-2xl text-black">
+          Design &nbsp; by{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+
+            href="https://github.com/rakibulanas777"
+          >
+            &nbsp; Rakibul Islam Anas
+          </a>
+        </p>
       </div>
     </div>
   );
